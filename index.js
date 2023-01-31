063908c2287ca783984e5ab6b1e899ed095a3d46
teeratchapat <teeratchapat.c@kkumail.com>
2023-01-31 18:07:52 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * @author Pamontep Panya
+ * @email pamontep.p@gmail.com
+ * @create date 2018-06-03 03:45:02
+ * @modify date 2018-06-03 03:45:02
+ * @desc A sample project of Node.js and Line API
+ */
+const server = require("express");
+const PORT = process.env.PORT || 9999;
+const request = require("request");
+const bodyParser = require("body-parser");
+
+server()
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .get("/", (req, res) =>
+    res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`)
+  )
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
