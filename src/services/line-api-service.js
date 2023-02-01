69afae24e215e8526d6be9d0d08f623358084636
teeratchapat <teeratchapat.c@kkumail.com>
2023-02-01 16:26:22 +0700

    feat: test

--- src/services/line-api-service.js
+++ src/services/line-api-service.js
@@ -1,44 +1,34 @@
-/**
- * @author Pamontep Panya
- * @email pamontep.p@gmail.com
- * @create date 2018-06-03 08:55:20
- * @modify date 2018-06-03 08:55:20
- * @desc A service for reply message via Line Messaging API.
- */
-const request = require("request");
-const apiToken =
-  "7oX36hL63bNEK+CVltL+yk+t2gdu/7eGSZOPGwzBFiHz4zd6PUzaZbaV5I0WLSWTyJN8K5BKHY8wy6Net9LU1277lM+ajMSV9v7vWYD3ZPrG0uDENUNtMkMw9Qge+WXhMcB7qLiml3dY+yfIMhZfIAdB04t89/1O/w1cDnyilFU=";
+import axios from "axios";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const uri =
+  "mongodb+srv://teeratchapat:Tee1234@cluster0.8sthelg.mongodb.net/test";
+
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db("bank_bot"); // database name
+});
+
 const apiRoute = "https://api.line.me/v2/bot/message/reply";
 const headers = {
   "Content-Type": "application/json",
-  Authorization: "Bearer " + apiToken,
+  Authorization: `Bearer ${process.env.API_TOKEN}`,
 };
 
-class LineAPIService {
-  constructor() {}
-
-  reply(replyToken, messages) {
-    return new Promise(function (resolve, reject) {
-      try {
-        let body = JSON.stringify({
-          replyToken: replyToken,
-          messages: messages,
-        });
-        return request.post(
-          {
-            url: apiRoute,
-            headers: headers,
-            body: body,
-          },
-          (err, res, body) => {
-            console.log("status = " + res.statusCode);
-            return resolve(res.statusCode);
-          }
-        );
-      } catch (e) {
-        return reject(e);
-      }
-    });
+const reply = async (replyToken, messages) => {
+  try {
+    const body = JSON.stringify({ replyToken, "messages" });
+    
+    const { status } = await axios.post(apiRoute, body, { headers });
+    console.log("status = " + status);
+    return status;
+  } catch (e) {
+    console.error(e);
+    throw e;
   }
-}
-module.exports = new LineAPIService();
+};
+
+export default reply;
