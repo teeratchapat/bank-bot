c2d7e91902fe90424e72a1dc94a25348f004b76a
teeratchapat <teeratchapat.c@kkumail.com>
2023-02-01 17:47:12 +0700

    feat: optimize code

--- src/services/line-api-service.js
+++ src/services/line-api-service.js
@@ -1,7 +1,9 @@
 const request = require("request");
 const MongoClient = require("mongodb").MongoClient;
-
 const { stringify } = JSON;
+
+require("dotenv").config();
+
 const apiToken = process.env.LINE_API_TOKEN;
 const apiRoute = "https://api.line.me/v2/bot/message/reply";
 const headers = {
@@ -9,42 +11,34 @@ const headers = {
   Authorization: `Bearer ${apiToken}`,
 };
 
-require("dotenv").config();
-
 const uri =
   "mongodb+srv://teeratchapat:Tee1234@cluster0.8sthelg.mongodb.net/test";
 
 class LineAPIService {
   db;
 
-  reply = async (replyToken, messages) => {
+  async reply(replyToken, messages) {
     try {
-      MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
-        if (err) return console.log(err);
-        this.db = client.db("bank_bot"); // database name
-        console.log("db connect success");
-      });
+      const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+      this.db = client.db("bank_bot");
+      console.log("DB connect success");
 
-      console.log(messages[0]["text"]);
-      messages[0]["text"] = "tee";
-
-      this.db?.collection("items").save(req.body, (err, result) => {
-        if (err) return console.log(err);
-        console.log("saved to database");
-      });
+      messages[0].text = "tee";
+      await this.db.collection("items").insertOne(messages[0]);
+      console.log("Saved to database");
 
       const body = stringify({ replyToken, messages });
-      const { statusCode } = await request.post({
-        url: apiRoute,
-        headers,
-        body,
-      });
-      console.log(`status = ${statusCode}`);
-      return statusCode;
-    } catch (e) {
-      throw e;
+      const response = await request.post({ url: apiRoute, headers, body });
+      console.log(`status = ${response.statusCode}`);
+      return response.statusCode;
+    } catch (error) {
+      throw error;
+    } finally {
+      if (this.db) {
+        this.db.close();
+      }
     }
-  };
+  }
 }
 
 module.exports = new LineAPIService();
