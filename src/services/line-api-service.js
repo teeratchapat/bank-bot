475aa8c8e5bc476e26959f135ead5129fe1f771e
teeratchapat <teeratchapat.c@kkumail.com>
2023-02-01 16:36:39 +0700

    feat: test

--- src/services/line-api-service.js
+++ src/services/line-api-service.js
@@ -1,35 +1,21 @@
-import axios from "axios";
-import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
-
-dotenv.config();
-
-const uri =
-  "mongodb+srv://teeratchapat:Tee1234@cluster0.8sthelg.mongodb.net/test";
-
-MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
-  if (err) return console.log(err);
-  db = client.db("bank_bot"); // database name
-});
-
+const request = require("request");
+const apiToken =
+  "7oX36hL63bNEK+CVltL+yk+t2gdu/7eGSZOPGwzBFiHz4zd6PUzaZbaV5I0WLSWTyJN8K5BKHY8wy6Net9LU1277lM+ajMSV9v7vWYD3ZPrG0uDENUNtMkMw9Qge+WXhMcB7qLiml3dY+yfIMhZfIAdB04t89/1O/w1cDnyilFU=";
 const apiRoute = "https://api.line.me/v2/bot/message/reply";
 const headers = {
   "Content-Type": "application/json",
-  Authorization: `Bearer ${process.env.API_TOKEN}`,
+  Authorization: `Bearer ${apiToken}`,
 };
 
-const reply = async (replyToken, messages) => {
-  try {
-    messages = "test";
-    const body = JSON.stringify({ replyToken, messages });
-
-    const { status } = await axios.post(apiRoute, body, { headers });
-    console.log("status = " + status);
-    return status;
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+const reply = (replyToken, messages) => {
+  const body = JSON.stringify({ replyToken, messages });
+  return new Promise((resolve, reject) => {
+    request.post({ url: apiRoute, headers, body }, (err, res) => {
+      if (err) return reject(err);
+      console.log("status = " + res.statusCode);
+      resolve(res.statusCode);
+    });
+  });
 };
 
-export default reply;
+module.exports = reply;
