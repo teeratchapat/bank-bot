9351b6a5041df4b402933c7eb219359d773fa49d
teeratchapat <teeratchapat.c@kkumail.com>
2023-02-01 17:15:05 +0700

    feat: test

--- src/services/line-api-service.js
+++ src/services/line-api-service.js
@@ -1,4 +1,6 @@
 const request = require("request");
+const MongoClient = require("mongodb").MongoClient;
+
 const { stringify } = JSON;
 const apiToken = process.env.LINE_API_TOKEN;
 const apiRoute = "https://api.line.me/v2/bot/message/reply";
@@ -8,10 +10,20 @@ const headers = {
 };
 
 require("dotenv").config();
+
+const uri =
+  "mongodb+srv://teeratchapat:Tee1234@cluster0.8sthelg.mongodb.net/test";
+
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db("bank_bot"); // database name
+  console.log("db connect success");
+});
 class LineAPIService {
   reply = async (replyToken, messages) => {
     try {
-      messages = "test";
+      console.log(messages);
+
       const body = stringify({ replyToken, messages });
       const { statusCode } = await request.post({
         url: apiRoute,
